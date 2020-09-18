f9f21a14d27871b2c39530b420de77145c4f4b60
lkarjun <lk.arjun@hotmail.com>
2020-09-18 09:23:44 +0530

    Working on StackOperation

--- InfixToPostfix.py
+++ InfixToPostfix.py
@@ -3,4 +3,22 @@ from typing import List, Dict
 class InfixTOPostfix:
     stack: List = []
     prefix: str = []
-    Operator: Dict = {'+': 1, '-': 1, '/': 2, '*': 2, '^': 3, '(': None, ')': None}
+    Operators: Dict = {'+': 1, '-': 1, '/': 2, '*': 2, '^': 3, '(': None, ')': None}
+
+    def __init__(self, value: str) -> None:
+        self.strings: str = value
+
+    def main(self) -> None:
+        for i in self.strings:
+            if i not in self.Operators: self.prefix += i
+            else: self.stackOperation(i)
+        
+        if len(self.stack) != 0:
+            for i in self.stack[::-1]: self.prefix +=i
+    
+    def stackOperation(self, s: str) -> str:
+        if len(self.stack) == 0:
+            self.push(s)
+
+        elif s == ')':
+            pass
